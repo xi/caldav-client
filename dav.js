9c4b3e5c86a95809470955a9632ba405aa716331
Tobias Bengfort <tobias.bengfort@posteo.de>
2022-02-04 18:01:35 +0100

    integrate with ical

---
+++ dav.js
@@ -0,0 +1,174 @@
+var uuid = function() {
+    if (crypto.randomUUID) {
+        return crypto.randomUUID();
+    }
+
+    var rnds = new Uint8Array(16);
+    crypto.getRandomValues(rnds);
+    rnds[6] = (rnds[6] & 0x0f) | 0x40;
+    rnds[8] = (rnds[8] & 0x3f) | 0x80;
+
+    var s = '';
+    for (var i = 0; i < 16; i++) {
+        s += (rnds[i] + 0x100).toString(16).substr(1);
+        if (i == 3 || i == 5 || i == 7 || i == 9) {
+            s += '-';
+        }
+    }
+    return s;
+};
+
+var date2idate = function(date, allDay, offset) {
+    var odate = offset ? new Date(date - offset) : date;
+    var idate = ICAL.Time.fromJSDate(odate);
+    if (allDay) {
+        idate.hour = 0;
+        idate.minute = 0;
+        idate.second = 0;
+        idate.isDate = true;
+    }
+    return idate;
+};
+
+var formatDate = function(date) {
+    return date
+        .toISOString()
+        .replace(/[-:.]/g, '')
+        .replace('000Z', 'Z');
+};
+
+export var getCalendars = function(url) {
+    return fetch(url, {
+        method: 'PROPFIND',
+        credentials: 'same-origin',
+        body: '<?xml version="1.0" encoding="utf-8"?>\n'
+            + '<D:propfind xmlns:D="DAV:">\n'
+            + '  <D:prop>\n'
+            + '    <D:resourcetype/>\n'
+            + '    <D:displayname/>\n'
+            + '    <A:calendar-color xmlns:A="http://apple.com/ns/ical/"/>\n'
+            + '  </D:prop>\n'
+            + '</D:propfind>',
+    }).then(function(response) {
+        if (response.ok) {
+            return response.text();
+        } else {
+            throw response;
+        }
+    }).then(function(xml) {
+        var parser = new DOMParser();
+        var dom = parser.parseFromString(xml, 'text/xml');
+        var calendars = [];
+        dom.querySelectorAll('response').forEach(response => {
+            if (response.querySelector('resourcetype calendar')) {
+                calendars.push({
+                    href: response.querySelector('href').textContent,
+                    name: response.querySelector('displayname').textContent,
+                    color: response.querySelector('calendar-color').textContent,
+                });
+            }
+        });
+        return calendars;
+    });
+};
+
+export var getEvents = function(href, info) {
+    return fetch(href, {
+        method: 'REPORT',
+        credentials: 'same-origin',
+        headers: {depth: '1'},
+        body: '<?xml version="1.0" encoding="utf-8"?>\n'
+            + '<L:calendar-query xmlns:L="urn:ietf:params:xml:ns:caldav">\n'
+            + '  <D:prop xmlns:D="DAV:">\n'
+            + '    <D:getcontenttype/>\n'
+            + '    <D:getetag/>\n'
+            + '    <L:calendar-data/>\n'
+            + '  </D:prop>\n'
+            + '  <L:filter>\n'
+            + '    <L:comp-filter name="VCALENDAR">\n'
+            + '      <L:comp-filter name="VEVENT">\n'
+            + `        <L:time-range start="${formatDate(info.start)}" end="${formatDate(info.end)}"/>\n`
+            + '      </L:comp-filter>\n'
+            + '    </L:comp-filter>\n'
+            + '  </L:filter>\n'
+            + '</L:calendar-query>',
+    }).then(function(response) {
+        if (response.ok) {
+            return response.text();
+        } else {
+            throw response;
+        }
+    }).then(function(xml) {
+        var parser = new DOMParser();
+        var dom = parser.parseFromString(xml, 'text/xml');
+        var events = [];
+        dom.querySelectorAll('response').forEach(response => {
+            // https://github.com/mozilla-comm/ical.js/wiki
+            var ics = response.querySelector('calendar-data').textContent;
+            var jcal = ICAL.parse(ics);
+            var comp = new ICAL.Component(jcal);
+            var vevent = new ICAL.Event(comp.getFirstSubcomponent('vevent'));
+            var iter = vevent.iterator();
+            var start = vevent.startDate.toJSDate();
+            var end = vevent.endDate.toJSDate();
+            var i;
+            while (i = iter.next()) {
+                var istart = i.toJSDate();
+                if (istart < info.start) {
+                    continue;
+                } else if (istart > info.end) {
+                    break;
+                }
+                events.push({
+                    groupId: response.querySelector('href').textContent,
+                    title: vevent.summary,
+                    offset: istart - start,
+                    start: istart,
+                    end: new Date(istart - (start - end)),
+                    allDay: vevent.startDate.isDate,
+                    comp: comp,
+                });
+            }
+        });
+        return events;
+    });
+};
+
+export var createEvent = function(info, source) {
+    var comp = new ICAL.Component(['vcalendar', [], []]);
+    var compEvent = new ICAL.Component('vevent');
+    comp.updatePropertyWithValue('prodid', '-//iCal.js Wiki Example');
+    comp.addSubcomponent(compEvent);
+
+    var vevent = new ICAL.Event(compEvent);
+    vevent.uid = uuid();
+
+    return {
+        groupId: source.id + vevent.uid + '.ics',  // FIXME: assumptions about href structure
+        title: 'new event',
+        offset: 0,
+        start: info.date,
+        allDay: info.allDay,
+        comp: comp,
+    };
+};
+
+export var commitEvent = function(data) {
+    var comp = data.extendedProps.comp;
+    var vevent = new ICAL.Event(comp.getFirstSubcomponent('vevent'));
+    vevent.summary = data.title;
+    vevent.startDate = date2idate(data.start, data.allDay, data.extendedProps.offset);
+    vevent.endDate = date2idate(data.end || data.start, data.allDay, data.extendedProps.offset);
+    return fetch(data.groupId, {
+        method: 'PUT',
+        credentials: 'same-origin',
+        body: comp.toString(),
+    });
+};
+
+export var deleteEvent = function(data) {
+    return fetch(data.groupId, {
+        method: 'DELETE',
+        credentials: 'same-origin',
+    });
+};
--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import * as config from './config.js';
+import * as dav from './dav.js';
+
 var calendar = new FullCalendar.Calendar(
     document.querySelector('.calendar'),
     {
@@ -6,6 +9,8 @@ var calendar = new FullCalendar.Calendar(
         scrollTime: '07:00',
         nowIndicator: true,
         weekNumberCalculation: 'ISO',
+        eventDrop: info => dav.commitEvent(info.event),
+        eventResize: info => dav.commitEvent(info.event),
         height: '100%',
         headerToolbar: {
             left: 'timeGridWeek,dayGridMonth',
@@ -16,3 +21,16 @@ var calendar = new FullCalendar.Calendar(
     }
 );
 calendar.render();
+
+dav.getCalendars(config.rootUrl).then(calendars => {
+    calendars.forEach(cal => {
+        calendar.addEventSource({
+            id: cal.href,
+            color: cal.color,
+            editable: true,
+            events: function(info, success, error) {
+                dav.getEvents(cal.href, info).then(success, error);
+            },
+        });
+    });
+});
